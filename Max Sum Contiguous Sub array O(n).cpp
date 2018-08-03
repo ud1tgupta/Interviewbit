d05a6a12d0f825f45293aafff1c2b36ed0118dc2
ud1tgupta <ud1tgupta@outlook.com>
2018-08-04 00:23:59 +0530

    Kadane's Algorithm - O(n)

--- Max Sum Contiguous Sub array O(n).cpp
+++ Max Sum Contiguous Sub array O(n).cpp
@@ -1,1 +1,9 @@
-
+int Solution::maxSubArray(const vector<int> &A) {
+    int max_current, max_global;
+    max_current = max_global = A.at(0);
+    for(auto i=A.begin()+1; i!=A.end(); i++){
+        max_current = max(*i,*i+max_current);
+        max_global = max(max_current,max_global);
+    }
+    return max_global;
+}
